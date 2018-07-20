a225a1d7afb65af25ea2cb3c1ba780eb666b15ed
jose marin <josemarinemail@gmail.com>
2018-07-20 01:33:58 -0400

    fixed some syntax highlighting and scrolling

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,71 @@
 (function() {
+    const md = `
+# Hello, World.
+------
+First off *why* did I build this when their are so many other markdown editors out there?...
+__Vim__ ftw that's why! And no other markdown editors out there have my must have vim keymapping \`imap kj <Esc>\`
+now without further ado
+Here is a list of the stuff you can do in markdown.
+
+## First the easy stuff
+
+* Lists!
+* in bullet
+* form
+
+### Also...
+
+1. numbered lists!
+2. in numerical
+3. form
+
+#### And then there was this
+
+1. Numerical bullets
+    * with sub bullets
+
+##### link all the things
+
+[Links!](http://www.google.com) Links to all sorts of stuff
+
+###### And finally
+
+![Friendly, non-threatening image of a brown bear](https://media.giphy.com/media/c9ndlj2AUhaqk/giphy.gif)
+
+But also don't forget...
+
+\`\`\`\javascript
+(function() {
+    return Math.floor(Math.random() * 100);
+})();
+\`\`\`\
+
+Also this
+
+\`\`\`html
+<!doctype html>
+<html lang="en-us">
+    <head>
+        <title>My Website</title>
+    </head>
+    <body></body>
+</html>
+\`\`\`
+
+And one of my personal favorites __tables__
+
+Simple | Markdown | Table
+--- | --- | ---
+*looks* | \`pretty\` | **good**
+    1 | 2 | 3
+
+And so on so forth.
+
+Happy writing in **VIM** mwahahaha!
+    `;
 
     if (!window.localStorage.getItem('markdown') || !window.localStorage.getItem('markdown').replace(/\s/g, '')) {
-        window.localStorage.setItem('markdown', '# Your markdown _here_')
+        window.localStorage.setItem('markdown', md)
     } 
 
     const editor = CodeMirror(document.querySelector('.js-input'), {
@@ -35,7 +99,7 @@
 
         document.querySelector('.CodeMirror-vscrollbar').addEventListener('scroll', event => {
             setTimeout(() => {
-                if ( document.querySelector('.js-content').parentElement.parentElement.scrollHeight - editor.getScrollInfo().top <= 700 ) {
+                if ( document.querySelector('.js-content').parentElement.parentElement.scrollHeight - editor.getScrollInfo().top <= 1000 ) {
                     document.querySelector('.js-content').parentElement.parentElement.scrollTop = 1000000;
                 }
                 else {
